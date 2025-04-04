5df5f2288f445f02b7f54dae605a38f0815f65aa
Saba-Sebiskveradze <ssebiskveradzee@gmail.com>
2025-04-04 19:52:57 +0400

    router
    change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 
 function App() {
